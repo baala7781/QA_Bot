c8b11055df1adef636548732861af563ba673aa5
S BALASAGAR GOUD <84790233+baala7781@users.noreply.github.com>
2024-07-28 23:59:02 +0530

    Update dummy_main.py

--- dummy_main.py
+++ dummy_main.py
@@ -15,9 +15,9 @@ import os
 import json
 import tempfile
 # from langchain_community import
-# api_key="AIzaSyAgb99S-u40oEh14jv-xD_jiqd3OGdp9Dc"
-os.environ["GOOGLE_API_KEY"] = "AIzaSyAgb99S-u40oEh14jv-xD_jiqd3OGdp9Dc"
-llm = GoogleGenerativeAI(model="models/text-bison-001")
+api_key=os.environ["GOOGLE_API_KEY"]
+
+llm = GoogleGenerativeAI(google_api_key=api_key,model="models/text-bison-001")
 
 def split_data(data):
     splitter= RecursiveCharacterTextSplitter(
