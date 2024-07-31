f8f0c01ef0bbe28329f4c7a6d394e8dd68d1eaa9
S BALASAGAR GOUD <84790233+baala7781@users.noreply.github.com>
2024-07-31 23:30:37 +0530

    Update dummy_main.py

--- dummy_main.py
+++ dummy_main.py
@@ -18,7 +18,7 @@ import tempfile
 # from langchain_community import
 # load_dotenv()
 # api_key=os.getenv("GOOGLE_API_KEY")
-os.getenv["GOOGLE_API_KEY"]="AIzaSyAgb99S-u40oEh14jv-xD_jiqd3OGdp9Dc"
+os.environ["GOOGLE_API_KEY"] = "AIzaSyAgb99S-u40oEh14jv-xD_jiqd3OGdp9Dc"
 llm = GoogleGenerativeAI(model="models/text-bison-001")
 
 def split_data(data):
